b7357d5a358d14eb15c40d1d7ae8986165969c17
holbrook <wanghaikuo@gmail.com>
2014-02-07 10:52:09 +0800

    import

---
+++ setup.py
@@ -0,0 +1,17 @@
+__author__ = 'holbrook'
+
+#  _   _       _ _                     _
+# | | | | ___ | | |__  _ __ ___   ___ | | __
+# | |_| |/ _ \| | '_ \| '__/ _ \ / _ \| |/ /
+# |  _  | (_) | | |_) | | | (_) | (_) |   <
+# |_| |_|\___/|_|_.__/|_|  \___/ \___/|_|\_\
+
+from distutils.core import setup
+setup(name='svnutil',
+      version='0.1',
+      description='A svn dump and restore(load) tool',
+      author='Holbrook',
+      author_email='wanghaikuo@gmail.com',
+      url='http://holbrook.github.com',
+      py_modules=['svnutil'],
+     )
---
+++ svnutil/svnaction.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+#  _   _       _ _                     _
+# | | | | ___ | | |__  _ __ ___   ___ | | __
+# | |_| |/ _ \| | '_ \| '__/ _ \ / _ \| |/ /
+# |  _  | (_) | | |_) | | | (_) | (_) |   <
+# |_| |_|\___/|_|_.__/|_|  \___/ \___/|_|\_\
+# wanghaikuo@gmail.com
+
+from optparse import OptionParser
+
+def exp(options, args):
+    parser = OptionParser()
+
+def imp(options, args):
+    parser = OptionParser()
